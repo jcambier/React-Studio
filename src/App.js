2a1c804a007fcf7a95849d5de50d622a779c5b57
Jared <jared_cambier@brown.edu>
2022-11-10 18:49:50 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -11,18 +12,34 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const [cartList, setCartList] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  function updateCart(itemName, price) {
+    const tempCartList = cartList;
+    tempCartList.push(itemName);
+    setCartList(tempCartList);
+    setTotalPrice(totalPrice + price);
+  }
 
   return (
     <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+      <h1 className="bakeryName">My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+        // <p>Bakery Item </p> // replace with BakeryItem component
+        <BakeryItem name={item.name} price={item.price} image={item.image} description={item.description} cartList={cartList} updateCart={updateCart}/>
       ))}
 
       <div>
         <h2>Cart</h2>
+        <div>
+          Total Price: {totalPrice}
+        </div>
         {/* TODO: render a list of items in the cart */}
+        {cartList.map((item) => (
+          <div>{item}</div>
+        ))}
       </div>
     </div>
   );
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,25 @@
 // TODO: create a component that displays a single bakery item
+import "../App.css";
+
+export default function BakeryItem(props) {
+
+    const cartList = props.cartList;
+    const name = props.name;
+    const image = props.image;
+    const description = props.description;
+    const price = props.price;
+
+    const handleClick = () => {
+        props.updateCart(name, price);
+    }
+
+    return (
+        <div className="container">
+            <h2>{name}</h2>
+            <img src={image}></img>
+            <p>{price}</p>
+            <p>Description: {description}</p>
+            <button onClick={handleClick}>Add to Cart</button>
+        </div>
+    );
+}
